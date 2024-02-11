2c39158584cbec5d4750a3c1c81be5b78211eebb
Himanshu verma <98012299+TechisHeaven@users.noreply.github.com>
2024-02-11 19:38:27 +0530

    vercel file

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const port = 3000;
 const userRoutes = require("./routes/userRoutes");
 const postRoutes = require("./routes/postRoutes");
 require("dotenv").config();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 ConnectDB();
 app.use(express.json());
 app.use(express.static(__dirname));
